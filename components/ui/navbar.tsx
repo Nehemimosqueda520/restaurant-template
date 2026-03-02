a92ec5435c358a934fd6fecbc54c0f9d96d8f257
nehemimosqueda520 <nehemimosqueda8@gmail.com>
2026-03-02 18:21:33 -0300

    toggle mode (dark /light)

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,19 +1,23 @@
+"use client";
+
 import { JSX } from "react/jsx-dev-runtime"
 import { Moon } from "@boxicons/react"
+import { Sun } from "lucide-react";
+import { useState } from "react";
 
 
 
 
 export function Navbar() {
-    
     interface navbarList  {
         Home: string,
         Menu: string,
         reservations: string,
         Contact: string,
         about: string,
-        moon: JSX.Element,
     }
+
+    /* moon has to a JSX element that when pressed toggles the dark mode of the website. */ 
 
     const navbarList: navbarList = {
         Home: "Home",
@@ -21,16 +25,28 @@ export function Navbar() {
         reservations: "Reservations",
         Contact: "Contact",
         about: "About",
-        moon: <Moon />,
-    }
+   }
+
+   
+   const [darkMode, setDarkMode] = useState(false);
+
+   
+
+ 
     return (    
-        <nav className="bg-secondary p-5 flex items-center justify-between w-full lg:pl-20 lg:pr-20 sm:pl-5 sm:pr-5 shadow-shadow">
-            <h1 className="text-3xl sm:text-2xl md:text-3xl font-poppins text-background ">DANLicious</h1>
+        <nav id="navbar" className="bg-secondary p-5 flex items-center justify-between w-full sm:w-full lg:pl-20 lg:pr-20 shadow-lg/10">
+            <h1 className="text-3xl sm:text-2xl md:text-3xl font-poppins text-text ">DANLicious</h1>
             <ul className="flex items-center gap-10">
                 {Object.entries(navbarList).map(([key, value]) => (
-                    <li key={key} className="text-background sm:text-sm md:text-base lg:text-lg font-poppins hover:text-primary transition-colors cursor-pointer">{value}</li>
+                    <li key={key}  className="text-text  sm:text-sm md:text-base lg:text-lg font-poppins hover:text-primary transition-colors cursor-pointer">{value}</li>
                 ))}
+                <div onClick={ () => {
+                    document.documentElement.classList.toggle("dark");
+                    setDarkMode(!darkMode);
+                }}>  {document.documentElement.classList.contains("dark") ? <Sun className="text-text hover:text-primary" /> : <Moon className="text-text hover:text-primary" />}
+                </div>
             </ul>
+            
         </nav>
     )
 }
